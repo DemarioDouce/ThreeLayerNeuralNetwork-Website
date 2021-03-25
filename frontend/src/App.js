c33a21cf874af7d8e833c5b66eaf5f6397827bcc
DemarioDouce <demario.douce567@gmail.com>
2021-03-24 21:18:50 -0400

    Changes and improvements.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Results from "./components/Results";
-import ParametersForm from "./components/ParametersForm";
+import Results from "./pages/Results";
+import ParametersForm from "./pages/ParametersForm";
 
 const App = () => (
   <BrowserRouter>
